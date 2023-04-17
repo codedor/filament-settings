aa80de2c8dfa98bc22b64ca4f2d016956dc7dfc9
De Langhe Thibo <38117009+thibodelanghe@users.noreply.github.com>
2023-04-17 10:34:26 +0200

    Test if form gets populated

--- tests/Unit/Pages/SettingsTest.php
+++ tests/Unit/Pages/SettingsTest.php
@@ -1,8 +1,39 @@
 <?php
 
+use Codedor\FilamentSettings\Drivers\DriverInterface;
 use Codedor\FilamentSettings\Pages\Settings;
+use Codedor\FilamentSettings\Repositories\SettingTabRepository;
+use Codedor\FilamentSettings\Tests\TestFiles\Settings\TestSettings;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Livewire\Livewire;
 
-it('mounts', function () {
-    Livewire::test(Settings::class);
+uses(RefreshDatabase::class);
+
+it('mounts with right values', function () {
+    $fakeValue = fake()->word;
+
+    /** @var SettingTabRepository $repo */
+    $repo = app(SettingTabRepository::class);
+
+    $repo->registerTab([
+        TestSettings::class,
+    ]);
+
+    /** @var \Codedor\FilamentSettings\Drivers\DriverInterface $settingsRepo */
+    $settingsRepo = app(DriverInterface::class);
+    $settingsRepo->set('site.name', $fakeValue);
+
+    $filamentState = Livewire::withQueryParams([
+        'tab' => '-test-settings-tab',
+    ])->test(Settings::class)
+        ->assertOk()
+        ->assertSee('site.name')
+        ->get('form')->getState();
+
+    expect($filamentState)->toBe([
+        'site' => [
+            'name' => $fakeValue,
+            'url' => null,
+        ],
+    ]);
 });
