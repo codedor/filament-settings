8c55654b32979174007412c9f31fc1e2c53cc934
Thomva <Thomva@users.noreply.github.com>
2023-04-26 14:04:40 +0000

    Fix styling

--- src/Widgets/RequiredFieldsWidget.php
+++ src/Widgets/RequiredFieldsWidget.php
@@ -9,7 +9,7 @@ class RequiredFieldsWidget extends Widget
 {
     protected static string $view = 'filament-settings::widgets.required_fields_widget';
 
-    protected int | string | array $columnSpan = 'full';
+    protected int|string|array $columnSpan = 'full';
 
     protected function getViewData(): array
     {
