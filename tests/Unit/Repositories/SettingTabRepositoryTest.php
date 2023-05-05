fb3c1e5f9b31d0baa18ba9ad51b7f3f4b0b3af6c
thibodelanghe <thibodelanghe@users.noreply.github.com>
2023-05-05 07:28:53 +0000

    Fix styling

--- tests/Unit/Repositories/SettingTabRepositoryTest.php
+++ tests/Unit/Repositories/SettingTabRepositoryTest.php
@@ -68,12 +68,12 @@ it('returns the schema for setting tabs', function () {
             Tab::make('Test Settings')
                 ->schema([
                     TextInput::make('site.name')
-                        ->default(fn() => null)
+                        ->default(fn () => null)
                         ->rules([
                             new SettingMustBeFilledIn,
                         ]),
                     TextInput::make('site.url')
-                        ->default(fn() => null),
+                        ->default(fn () => null),
                 ]),
         ]);
 });
